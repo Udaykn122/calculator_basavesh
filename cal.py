0e8e265e5afaf83613693ac5cf54c240725e94b5
basavesh48 <basavesh.m@s.amity.edu>
2025-07-09 23:20:29 +0530

    added last part of code

--- cal.py
+++ cal.py
@@ -29,3 +29,13 @@ def calculator():
             except ValueError:
                 print("Invalid input. Please enter numbers only.")
                 continue
+             if choice == '1':
+                print(f"Result: {add(num1, num2)}")
+            elif choice == '2':
+                print(f"Result: {subtract(num1, num2)}")
+            elif choice == '3':
+                print(f"Result: {multiply(num1, num2)}")
+            elif choice == '4':
+                print(f"Result: {divide(num1, num2)}")
+        else:
+            print("Invalid input. Please enter a number between 1 and 5.")
